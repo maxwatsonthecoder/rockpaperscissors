ecd335ccd40b74afb162e37bf9dba4e5f0a30fc4
maxwatsonthecoder <140012390+maxwatsonthecoder@users.noreply.github.com>
2024-10-13 23:50:27 -0500

    Add base UI and DOM connections

--- javascript.js
+++ javascript.js
@@ -3,56 +3,72 @@ let userWins = 0;
 let userLoses = 0;
 let ties = 0;
 
+const rockBtn = document.getElementById(rock-btn)
+const paperBtn = document.getElementById(paper-btn)
+const scissorsBtn = document.getElementById(scissors-btn)
+
+function chooseRock() {
+    console.log("clicked rock button!")
+}
+
+function choosePaper() {
+    console.log("clicked paper button!")
+}
+
+function chooseScissors() {
+    console.log("clicked scissors button!")
+}
+
 for (let round = 1; round <= 5; round++) {
    
 
-// const getUserChoice = prompt("Choose Rock, Paper, or Scissors.").toLowerCase();
-if (getUserChoice === "rock" || getUserChoice === "paper" || getUserChoice === "scissors") {
-    console.log("You chose: " + getUserChoice);
-} else {
-    console.log("Invalid choice, perhaps a typo? Please choose, rock, paper, or scissors without spaces.");
-}
-
-function getComputerChoice() {
-    const randomCompValue = Math.random();
-    if (randomCompValue < 0.33) {
-        return "rock";
-    } else if (randomCompValue < 0.66) {
-        return "paper";
+    const getUserChoice = prompt("Choose Rock, Paper, or Scissors.").toLowerCase();
+    if (getUserChoice === "rock" || getUserChoice === "paper" || getUserChoice === "scissors") {
+        console.log("You chose: " + getUserChoice);
     } else {
-        return "scissors";
-    }
-}
- 
-const computerSelection = getComputerChoice();
-
-const userSelection = getUserChoice;
-
-
-function playRound(userSelection, computerSelection) {
-    if (userSelection === computerSelection) {
-        ties++;
-        return "It's a tie!";
-       
-    } else if (
-        (userSelection === "rock" && computerSelection === "scissors") ||
-        (userSelection === "paper" && computerSelection === "rock") ||
-        (userSelection === "scissors" && computerSelection === "paper") 
-    ) {
-        userWins++;
-        return "You win!";
-    } else {
-        userLoses++;
-        return "You lose!";
+        console.log("Invalid choice, perhaps a typo? Please choose, rock, paper, or scissors without spaces.");
     }
 
-}
-console.log("Computer chose: " + computerSelection);
+    function getComputerChoice() {
+        const randomCompValue = Math.random();
+        if (randomCompValue < 0.33) {
+            return "rock";
+        } else if (randomCompValue < 0.66) {
+            return "paper";
+        } else {
+            return "scissors";
+        }
+    }
+    
+    const computerSelection = getComputerChoice();
+
+    const userSelection = getUserChoice;
+
+
+    function playRound(userSelection, computerSelection) {
+        if (userSelection === computerSelection) {
+            ties++;
+            return "It's a tie!";
+        
+        } else if (
+            (userSelection === "rock" && computerSelection === "scissors") ||
+            (userSelection === "paper" && computerSelection === "rock") ||
+            (userSelection === "scissors" && computerSelection === "paper") 
+        ) {
+            userWins++;
+            return "You win!";
+        } else {
+            userLoses++;
+            return "You lose!";
+        }
+
+    }
+    console.log("Computer chose: " + computerSelection);
 
 
 
-const roundResult = playRound(userSelection, computerSelection);
-console.log(roundResult);
+    const roundResult = playRound(userSelection, computerSelection);
+    console.log(roundResult);
 
 }
 
